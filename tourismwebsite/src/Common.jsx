36abccf720f0382cf6d66b56d58a253647cf44b6
Shaurya Srivastava <shauryasris21hcs@gmail.com>
2021-07-18 23:11:44 +0530

    contact form updated

--- tourismwebsite/src/Common.jsx
+++ tourismwebsite/src/Common.jsx
@@ -6,7 +6,7 @@ const Common = (props) => {
   return (
     <>
       <section id="header" className="">
-        <div className="container-fluid nav_bg">
+        <div className="container-fluid ">
           <div className="row">
             <div className="col-10 mx-auto">
               <div className="row">
@@ -16,7 +16,7 @@ const Common = (props) => {
                   </h1>
                   <h2 className="my-3">{props.name} </h2>
                   <div className="mt-3">
-                    <NavLink to={props.visit} className="btn-get-started">
+                    <NavLink to={props.visit} className="btn btn-outline-primary">
                       {props.btname}
                     </NavLink>
                   </div>
